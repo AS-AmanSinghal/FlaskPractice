c8761293b8123c65faa818bfb35cb1af32c35e3c
Aman Singhal <amansinghal.it1@gmail.com>
2021-04-29 01:46:44 +0530

    Create Routes.

--- server.py
+++ server.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, RS!'
+
+
+@app.route('/blog')
+def blog():
+    return 'This is my Blog Page'
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'This is my Dog Blog Page'
