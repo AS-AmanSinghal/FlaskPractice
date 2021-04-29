9a364834dfce78468be54bf3343dd53c05734fa8
Aman Singhal <amansinghal.it1@gmail.com>
2021-04-30 00:55:13 +0530

    Render Html.

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, RS!'
+    return render_template('index.html')
+    # return 'Hello, AMANSARS!'
 
 
-@app.route('/blog')
+@app.route('/about.html')
 def blog():
-    return 'This is my Blog Page'
+    return render_template('about.html')
 
 
 @app.route('/blog/2020/dogs')
 def blog2():
     return 'This is my Dog Blog Page'
+
+
+@app.route('/blog/2020/fuck')
+def blog3():
+    return 'This is FUCK'
+
+
+@app.route('/urltest/<username>/<int:post_id>')
+def urlTest(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
